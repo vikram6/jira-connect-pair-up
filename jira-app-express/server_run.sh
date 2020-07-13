0f7aafb1e8392d5ec763fb70d5af1daec5d96bb4
revanth <aniredr@amazon.com>
2020-07-12 21:26:05 -0500

    ssm params

--- jira-app-express/server_run.sh
+++ jira-app-express/server_run.sh
@@ -4,7 +4,7 @@
 
 sed -i "s/<IP_HERE>/$(curl http://169.254.169.254/latest/meta-data/public-ipv4)/" config.json
 
-jiraapigwkey=$(aws ssm get-parameter --name "jiraapigwkey" --query "Parameter.Value" --output text)
+jiraapigwkey=$(aws ssm get-parameter --name "jiraapigwkey" --query "Parameter.Value" --output text --region us-east-1)
 
 export JIRAAPIGWKEY=$jiraapigwkey
 
